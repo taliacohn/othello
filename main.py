63632015c94a47fc03854456327f614405302ccc
taliacohn <taliacohn3@gmail.com>
2022-07-28 09:21:00 -0400

    Main Game Page

---
+++ main.py
@@ -0,0 +1,9 @@
+from view.game_console_view import GameConsoleView
+from controller.game_controller import GameController
+from model.othello_game import OthelloGame
+
+model = OthelloGame(board_size=8)
+view = GameConsoleView(model)
+controller = GameController(view, model)
+
+controller.run_game()
